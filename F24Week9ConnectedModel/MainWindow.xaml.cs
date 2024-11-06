28142fb6afa46da378bc839da0d9c990efba976e
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-11-06 17:35:06 -0500

    load data

--- F24Week9ConnectedModel/MainWindow.xaml.cs
+++ F24Week9ConnectedModel/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace F24Week9ConnectedModel
 {
@@ -16,9 +19,41 @@ namespace F24Week9ConnectedModel
     /// </summary>
     public partial class MainWindow : Window
     {
+        // read the connection string from app.config
+        string connStr = ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "select EmployeeID, FirstName, LastName, City, Country from Employees";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                DataTable tbl = new DataTable();
+                tbl.Load(reader);
+
+                grdEmployees.ItemsSource = tbl.DefaultView;
+            }
+            //conn.Close();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
